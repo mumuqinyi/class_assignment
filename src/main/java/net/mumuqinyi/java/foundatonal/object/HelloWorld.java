c8a4f79287131d3d8f0925b8fdb782d791a0e188
mumuqinyi <2920112104@qq.com>
2021-09-20 14:47:07 +0800

    企业级配置学习

---
+++ src/main/java/net/mumuqinyi/java/foundatonal/object/HelloWorld.java
@@ -0,0 +1,15 @@
+package net.mumuqinyi.java.foundatonal.object;
+
+/**
+ * 关于maven/git/模块化学习的运行实验
+ *
+ * @author tong 2920112104@qq.com
+ * @version 2021/9/20 14:37
+ * @since JDK17
+ */
+public class HelloWorld {
+    public static void main(String[]args)
+    {
+        System.out.println("Hello World with Windows10 &JDK17 & Git & Maven & ");
+    }
+}
